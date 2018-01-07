c8e4a62b8262abd85d8a0e63912fb859b899aae4
nmastracci <mastracci.natalie@gmail.com>
2018-01-07 11:50:53 -0500

    adding server functionality

--- admin/scripts/connect.php
+++ admin/scripts/connect.php
@@ -1,10 +1,14 @@
 <?php
-	$user = "root";
-	$pass = "root";
-	$url = "localhost";
-	$db = "db_nammie";
+$user = "nmastrac_alexand";
+	$pass = "PotatoTomato4$";
+	$url = "gator4091.hostgator.com";
+	$db = "nmastrac_db_nammie";
+	// $user = "root";
+	// $pass = "root";
+	// $url = "localhost";
+	// $db = "db_nammie";
 
-	$link = mysqli_connect($url, $user, $pass, $db, "8888"); 
+	$link = mysqli_connect($url, $user, $pass, $db); 
 
 	if(mysqli_connect_errno()) {
 		printf("Connection Failed: %s\n", mysqli_connect_error());
