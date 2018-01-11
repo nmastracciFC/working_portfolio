1d353bb8f7dd08237cc4aa00f35ff3fbcb76a863
nmastracci <mastracci.natalie@gmail.com>
2018-01-11 00:34:46 -0500

    ipad done

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if (isset($_POST['name'])) {
 				<a href="https://twitter.com/NMastracci"><img class="twitter" src="images/social-twitter.png" alt="twitter logo"></a>
 				<a href="https://www.linkedin.com/in/natalie-mastracci-b8772257/"><img class="linkedIn" src="images/social-linkedin.png" alt="linkedIn logo"></a>
 				
-				
+
 			</div> -->
 
 			<ul class="main-nav">
@@ -62,8 +62,8 @@ if (isset($_POST['name'])) {
  			<div id="heading">
  				<h1 >Natalie Mastracci</h1>
  				<h3>After spending nine years facing backward in a boat it’s time to look forward.</h3>
-				<h4 id="to-portfolio">Go to Portfolio &gt;</h4>
-				<h4 id="to-contact" >Keep in Touch  &gt;</h4>
+				<h4 id="to-portfolio">Go to <strong>Portfolio &gt;</strong></h4>
+				<h4 id="to-contact" >Keep <strong>in Touch  &gt;</strong></h4>
 			</div>
 			<img id="eight" src="images/eight-nowhite.png" alt="eight rowing">
 
@@ -72,8 +72,8 @@ if (isset($_POST['name'])) {
 				<!-- <h2>Designer &amp; Developer</h2> -->
 				<h2>Full Stack Developer</h2>
 				<h3>I believe efficiency, functionality and beauty are created equal but above all else make something great.</h3>
-				<!-- <h4>Areas of study: procedural and OOP PHP, mySQL database architecture, MVC frameworks (ex. Laravel, codeigniter), core Javascipt and popular JS libraries (such as greensock, D3.js and three.js), responsive web design with media queries (as well as foundation and bootstrap), cinema4D and the adobe creative cloud suite.</h4> -->
-				<ul>
+				<h4>Areas of study: procedural and OOP PHP, mySQL database architecture, MVC frameworks (ex. Laravel, codeigniter), core Javascipt and popular JS libraries (such as greensock, D3.js and three.js), responsive web design with media queries (as well as foundation and bootstrap), cinema4D and the adobe creative cloud suite.</h4>
+				<!-- <ul>
 					Areas of study: 
 					<li>procedural and OOP PHP,</li>
 					<li>mySQL database architecture,</li>
@@ -81,7 +81,7 @@ if (isset($_POST['name'])) {
 					<li>core Javascipt and popular JS libraries (greensock, D3.js and three.js),</li>
 					<li>responsive web design with media queries (as well as foundation and bootstrap),</li>
 					<li>cinema4D and the adobe creative cloud suite.</li>    
-				</ul>
+				</ul> -->
 	
 			</div>
 		</header>
